80ca5cf96d3ea62b9882350f6a8bfc280d3b8f62
handzj2 <handzj2@gmail.com>
2026-02-05 08:52:08 +0300

    Delete frontend_disabled directory

--- frontend_disabled/lib/api/index.ts
+++
@@ -1,7 +0,0 @@
-// Barrel export for all API utilities
-export { api, default } from './client';
-export * from './client'; // Re-export all named exports
-
-// You'll add service exports here later
-// export * from './services/auth.service';
-// export * from './services/client.service';
--- frontend_disabled/lib/api/services/index.ts
+++
@@ -1,5 +0,0 @@
-// Export all services from here
-export { authService } from './auth.service';
-// Add more as you create them:
-// export { clientService } from './client.service';
-// export { loanService } from './loan.service';
